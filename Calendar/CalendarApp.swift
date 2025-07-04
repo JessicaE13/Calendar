df5f4fb4b42a6bddd65c91006fe382cb5a4cdd94
Jessica Estes <jessica.m.estes@gmail.com>
2025-07-04 14:28:21 -0400

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Jessica Estes on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
